66391671b0a7d1c4e9edfaef30f25eb5d938c5c7
Tejeshwar-Dev <tejeshwarsingh.g.dev+github@gmail.com>
2024-03-10 00:32:55 +0530

    update product cart UI and code cleanup

--- src/Products/ProductCard/ProductCard.js
+++ src/Products/ProductCard/ProductCard.js
@@ -33,65 +33,29 @@ function ProductCard({id, productImg, price, title, description, region, cardSty
         updateWishlist(id);
     }
 
-    // const itemQuantity = cartItems.find(item => item.id === id);
-    // const addToCartHtml = `
-    //     <button className={styledClasses['remove-item']} onClick={(event) => updateToCart(event, {type: 'REMOVE_FROM_CART', id: id})}>-</button>
-    //         <label>0</label>
-    //     <button className={styledClasses['add-item']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>+</button>
-    // `;
+    const cartItem = cartItems.find(item => item.id === id);
+    let productCardCartBtn = (
+        <button className={styledClasses['product-cart-btn']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>Add to cart</button>
+    );
     
-    // const addToCart = () => {
-        const cartItem = cartItems.find(item => item.id === id);
-        let productCardCartBtn = (
-            <button className={styledClasses['product-cart-btn']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>Add to cart</button>
-        );
-        
-        if(cartItem && cartItem.quantity > 0) {
-            productCardCartBtn = (
-                <div className={styledClasses['product-cart-quantity-btn']}>
-                    <button className={styledClasses['remove-item']} onClick={(event) => updateToCart(event, {type: 'REMOVE_FROM_CART', id: id})}>-</button>
-                        <label className={styledClasses['product-quantity']}>{cartItem.quantity}</label>
-                    <button className={styledClasses['add-item']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>+</button>
-                </div>
-            )
-        }
-        
-
-        // return productCardCartBtn;
-    // };
+    if(cartItem && cartItem.quantity > 0) {
+        productCardCartBtn = (
+            <div className={styledClasses['product-cart-quantity-btn']}>
+                <button className={styledClasses['remove-item']} onClick={(event) => updateToCart(event, {type: 'REMOVE_FROM_CART', id: id})}>-</button>
+                    <label className={styledClasses['product-quantity']}>{cartItem.quantity}</label>
+                <button className={styledClasses['add-item']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>+</button>
+            </div>
+        )
+    }
     
     return (
-        // <div id={id} className={styledClasses["card-container"]} style={cardStyle} onClick={() => showProductInfoFn(id)}>
-        //     <section className={styledClasses["card-header"]}>
-        //         <img className={styledClasses['product-image']} src={productImg} alt="prodduct_img"/>
-        //         {/* <button  className={styledClasses['product-wishlist-btn']} onClick={(event) => updateItemToWishlist(event, id)}>
-        //             <img src={wishedItems.includes(id) ? HEART_FILLED_IMG : HEART_IMG} alt="favourite" />
-        //             Wishlist
-        //         </button> */}
-        //     </section>
-        //     <div className={styledClasses["card-header-divider"]}>
-        //         <div className={styledClasses["card-divider-blank"]}></div>
-        //         <p className={styledClasses["card-price"]}>${price}</p>
-        //     </div>
-        //     <div className={styledClasses["card-body"]}>
-        //         <h3 className={styledClasses["card-title"]}>{title}</h3>
-        //         <p className={styledClasses["card-subtitle"]}>{region}</p>
-        //     </div>
-        //     <footer className={styledClasses['product-card-footer']}>
-        //         {productCardCartBtn}
-        //         {/* <button className={styledClasses['remove-item']} onClick={(event) => updateToCart(event, {type: 'REMOVE_FROM_CART', id: id})}>-</button>
-        //         <label>0</label>
-        //         <button className={styledClasses['add-item']} onClick={(event) => updateToCart(event, {type: 'ADD_TO_CART', id: id})}>+</button> */}
-        //     </footer>
-        // </div>
-
         <div className={styledClasses['product-card']} onClick={() => showProductInfoFn(id)}>
             <div className={styledClasses['product-card-header']}>
                 <div className={styledClasses['product-img-wrapper']}>
                     <img src={productImg} alt={title}/>
                     <p className={styledClasses['product-price']}>{price}</p>
                 </div>
-                <div className={styledClasses['product-wishlist']}>
+                <div className={styledClasses['product-wishlist']} onClick={(event) => updateItemToWishlist(event, id)}>
                     <img src={wishedItems.includes(id) ? HEART_FILLED_IMG : HEART_IMG} alt="favourite" />
                 </div>
             </div>
